1ef7ccd1b034e8e2665944e74c42e12ff93dba45
Phyllis Kan <pyk9q7@gmail.com>
2019-05-19 22:03:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,8 @@ end
 def array_with_two_elements
   my_two_array = ["dog", "alligator"]
 end
+
+def first_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift[1]
+end
