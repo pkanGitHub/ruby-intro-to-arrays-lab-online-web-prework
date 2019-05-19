a5b0acf25cdcad92ec3bb7c62997d61f9ba49b5d
Phyllis Kan <pyk9q7@gmail.com>
2019-05-19 22:22:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,6 @@ def array_with_two_elements
   my_two_array = ["dog", "alligator"]
 end
 
-def first_element(array, index_number)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+def first_element(ndex_number)
   taylor_swift[0]
 end
